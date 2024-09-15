8dc4d35d8196018fdc4d9e9cb5d28aabb74b041f
Vishvamsinh Vaghela <vaghelavishvamsinh11111@gmail.com>
2024-09-15 11:24:30 +0530

    gewgew

--- tests/readAndWriteJson.test.js
+++ tests/readAndWriteJson.test.js
@@ -75,5 +75,5 @@ describe('writeJSON', () => {
     expect(fs.writeFile).toHaveBeenCalledWith(writePath, JSON.stringify(jsonObject));
   });
 
-  console.log("GxEffewgegeflo")
+  console.log("GxEffewgelo")
 });
