276e52365d9047fd07dfe42c3272a3b4971aeb52
Vishvamsinh Vaghela <vaghelavishvamsinh11111@gmail.com>
2024-11-22 12:07:46 +0530

    hello test

---
+++ tests/hello.test.js
@@ -0,0 +1,9 @@
+const hello = require('../scripts/hello')
+
+describe('hello function', () => {
+
+    it('should say hello', async () => {
+        let x = hello()
+        expect(x).toBe("hello")
+    });
+});
